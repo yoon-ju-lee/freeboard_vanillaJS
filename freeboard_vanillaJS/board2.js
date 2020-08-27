30d276329a41f6ad84445d8339cb64a90ad5a312
yoon-ju-lee <tenbillionandmemory@gmail.com>
2020-08-27 20:43:34 +0900

    modify files

--- freeboard_vanillaJS/board2.js
+++ freeboard_vanillaJS/board2.js
@@ -18,17 +18,38 @@ function makeTable(){
         tr.style.height='25px';
         tr.style.marginLeft='150px'; 
         tr.style.textAlign='center';
-        
+
+
+
             let td=document.createElement('td');
             td.style.border='1px dotted black';
             td.style.width='30px';
             td.textContent=item.index+1;
             tr.appendChild(td);
+
+            let number=item.index+1;
+
+            // for(let i=0; i<parsedList.length; i++){
+            //     let file=
+            // }    
+
             let td2=document.createElement('td');
             td2.style.border='1px dotted black';
             td2.style.width='250px';
-            td2.textContent=item.title;
+            //td2.textContent=item.title;
+
+            let alink=document.createElement('a');
+            alink.href=`${item.index}.html`;
+            alink.title='move';
+            //alink.style.color='black';
+            alink.style.textDecoration='none';
+            alink.textContent=item.title;
+            td2.appendChild(alink);
+
             tr.appendChild(td2);
+
+            
+
             let td3=document.createElement('td');
             td3.style.border='1px dotted black';
             td3.style.width='250px';
